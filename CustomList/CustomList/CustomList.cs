f1922b4042acaa8352742830f56681bf0a616e52
gjjuliex <12baysidejgjx@gmail.com>
2018-10-08 10:56:13 -0500

    working on some functions and added add method unit tests

--- CustomList/CustomList/CustomList.cs
+++ CustomList/CustomList/CustomList.cs
@@ -6,19 +6,102 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    public class CustomList<T>
+    class CustomList<T>
     {
-       private T[] items = new T[1];
-        //count goes here
+        private T[] arr = new T[11];
+       
+
+        public T this[int i]
+        {
+            get { return arr[1]; }
+            set { arr[i] = value; }
+        }
+
+        private T[] items = new T[1];
+        private CustomList<T> customList;
+
+        public CustomList(CustomList<T> customList)
+        {
+            this.customList = customList;
+        }
+
+        //COUNT//
+        public int Count(CustomList<int> index)
+        {
+            return index.Count();
+
+        }
+        //ADD//
         public void Add(int value)
+
+        {
+            T[] temp = new T[capacity];
+            for (int i = 0; i < Count; i++)
+            {
+                temp[i] = items[i];
+            }
+            temp[Count] = value;
+            Count++;
+            items = temp;
+
+
+        }
+        //REMOVE//
+        public void Remove(int value)
+        {
+            int[] customList = new int[customList.Length - 1];
+            int i = 0;
+            while (i < customList.Length)
+            {
+                if (i != Remove)
+                {
+
+                }
+            }
+
+        }
+        /*public int [] RemoveInt(int [], CustomList, int RemoveAt)
+        {
+            customerList.RemoveAt(index);
+        }*/
+
+
+
+
+
+
+        //TO STRING//
+        public void ToString()
+        {
+
+        }
+
+        //Zip//
+        public void Zip()
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+            var list1AndList2 =
+        }
+        //Overload +//
+        /*public void Overload()
+        {
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+
+            List<int> list3 = list1 + list2
+        }*/
+
+        public static CustomList<T> operator +(CustomList<T> list1, CustomList<T> list2)
         {
             
+            CustomList<T> newList = new CustomList<T>(list1 + list2);
+            newList = list1 + list2;
+            return newList;
         }
 
-        public void ToArray(int value)
-        {
+        //Overload -//
 
-        }
-
+      
     }
 }
--- CustomList/UnitTestProject1/UnitTest1.cs
+++ CustomList/UnitTestProject1/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodAdd1()
         {
             // Arrange
             CustomList<int> customList = new CustomList<int>();
@@ -21,7 +21,7 @@ namespace UnitTestProject1
             Assert.AreEqual(value, customList[0]);
         }
         [TestMethod]
-        public void TestMethod2()
+        public void TestMethodAdd2()
         {
             // Arrange
             CustomList<int> customList = new CustomList<int>();
@@ -36,6 +36,64 @@ namespace UnitTestProject1
             Assert.AreEqual(value1, customList[1]);
 
         }
+        [TestMethod]
+        public void TestMethodAdd3()
+        {
+            // Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 2;
+            int value1 = 4;
+            int value2 = 6;
 
-    }
-}
+            // Act
+            customList.Add(value);
+            customList.Add(value1);
+            customList.Add(value2);
+
+            // Assert
+            Assert.AreEqual(value2, customList[2]);
+
+        }
+        [TestMethod]
+        public void TestMethodAdd4()
+        {
+            // Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 2;
+            int value1 = 4;
+            int value2 = 6;
+            int value3 = 8;
+
+            // Act
+            customList.Add(value);
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+
+            // Assert
+            Assert.AreEqual(value3, customList[3]);
+
+        }
+        [TestMethod]
+        public void TestMethodAdd5()
+        {
+            // Arrange
+            CustomList<int> customList = new CustomList<int>();
+            int value = 2;
+            int value1 = 4;
+            int value2 = 6;
+            int value3 = 8;
+            int value4 = 10;
+
+            // Act
+            customList.Add(value);
+            customList.Add(value1);
+            customList.Add(value2);
+            customList.Add(value3);
+            customList.Add(value4);
+
+            // Assert
+            Assert.AreEqual(value4, customList[4]);
+
+        }
+
